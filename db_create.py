d625f34646a528238fd8376df9cafd51437c274e
Jakub Janicki <janicus321@gmail.com>
2023-10-17 23:18:02 +0200

    new database create script using stockfish 16

--- db_create.py
+++ db_create.py
@@ -8,7 +8,10 @@ import tqdm
 from multiprocessing import Pool
 import time
 import io
-
+from mcts_node import Node
+from db_dataloader import db_dataloader
+import stockfish
+from cache_read_priority import Cache
 
 
 def get_centipawns(prob):
@@ -19,11 +22,11 @@ def map_centipawns_to_probability(centipawns):
     return math.atan2(centipawns, 111.714640912) / 1.5620688421
 
 
-def insert_or_replace_position(conn, fen, cp, prob):
+def insert_or_replace_position(conn, fen, cp, prob, encoded):
     # Insert or replace the position, keeping the one with the closest 'cp' to 0
     conn.execute("""
-        INSERT OR REPLACE INTO positions (fen, cp, prob)
-        VALUES (?, ?, ?)
+        INSERT OR REPLACE INTO positions (fen, cp, prob, encoded)
+        VALUES (?, ?, ?, ?)
         ON CONFLICT(fen) DO UPDATE
         SET 
             cp = CASE
@@ -42,10 +45,20 @@ def insert_or_replace_position(conn, fen, cp, prob):
                 WHEN ABS(positions.prob) < ABS(excluded.prob) THEN positions.prob
                 ELSE excluded.prob
             END
-    """, (fen, cp, prob))
+    """, (fen, cp, prob, encoded))
+
+
+def insert_or_abort(conn, fen, cp, prob, encoded):
+    # Insert or replace the position, keeping the one with the closest 'cp' to 0
+    conn.execute("""
+        INSERT OR ABORT INTO positions (fen, cp, prob, encoded)
+        VALUES (?, ?, ?, ?)
+    """, (fen, cp, prob, encoded))
+
 
 def get_position_count(conn):
     return conn.execute("SELECT COUNT(*) FROM positions").fetchone()[0]
+
 
 def safe_commit(conn):
     for retry in range(10):
@@ -88,7 +101,7 @@ def shard_parser(shard_path) -> None:
     conn = sqlite3.connect('C:/sqlite_chess_db/chess_positions.db')
     shard_name = os.path.basename(shard_path)
     pbar = tqdm.tqdm(total=100000000,
-                        desc="Processing shard " + shard_name + ".")
+                     desc="Processing shard " + shard_name + ".")
     eval_games_count = 0
     with open(shard_path) as file:
         # select until "1." is found
@@ -104,33 +117,108 @@ def shard_parser(shard_path) -> None:
         conn.commit()
         conn.close()
 
+def adapt_array(arr):
+    """
+    http://stackoverflow.com/a/31312102/190597 (SoulNibbler)
+    """
+    out = io.BytesIO()
+    np.save(out, arr)
+    out.seek(0)
+    return sqlite3.Binary(out.read())
+
+
+def convert_array(text):
+    out = io.BytesIO(text)
+    out.seek(0)
+    return np.load(out)
+
+
+def db_parser():
+    conn = sqlite3.connect('C:/sqlite_chess_db/chess_positions.db')
+    table_name = "positions"
+    cursor = conn.cursor()
+    sqlite3.register_adapter(np.ndarray, adapt_array)
+    sqlite3.register_converter("array", convert_array)
+    # select all rows where "encoded" is not null
+    id, fen, cp, prob, encoded = cursor.execute(
+        "SELECT * FROM {}".format(table_name)).fetchone()
+    encoded = convert_array(encoded)
+    print(encoded.shape)
+    
+    
+    id_count = get_position_count(conn)
+    for idx in tqdm.trange(id_count):
+        idx = idx + 1
+        id, fen, cp, prob, encoded = cursor.execute(
+            "SELECT * FROM {} WHERE id = {}".format(table_name, idx)).fetchone()
+        # x = conn.execute(
+        #         f"SELECT * FROM positions LIMIT {10} OFFSET {0}")
+        board = chess.Board(fen)
+        node = Node(1, board, {}, None, None)
+        insert_or_replace_position(conn, fen, cp, prob, node.encoded)
+        if idx % 500 == 0:
+            conn.commit()
+    conn.commit()
+
+
+
+def create_new(total_time):
+    start_pos = chess.Board()
+    start_time = time.time()
+    conn = sqlite3.connect('C:/sqlite_chess_db/chess_positions.db')
+    cursor = conn.cursor()
+    stockfish_engine = stockfish.Stockfish(
+        path="D:/stockryba/stockfish-windows-x86-64-avx2.exe",
+        depth=20,
+        parameters={
+            'Threads': 4,
+        }
+    )
+    print(stockfish_engine.get_parameters())
+    # create the table
+    sqlite3.register_adapter(np.ndarray, adapt_array)
+    sqlite3.register_converter("array", convert_array)
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS positions (
+            id INTEGER PRIMARY KEY,
+            fen TEXT NOT NULL,
+            cp INTEGER NOT NULL,
+            prob REAL NOT NULL,
+            encoded array NOT NULL
+        )
+    """)
+    conn.commit()
+    pos_count = 0
+    pbar = tqdm.tqdm()
+    cache = Cache(8192)
+    
+    while start_time + total_time > time.time():
+        # play a game
+        board = chess.Board()
+        node = Node(1, board, {}, None, None)
+        while not board.is_game_over(claim_draw=True):
+            board.push(np.random.choice(list(board.legal_moves)))
+            fen = board.fen()
+            if fen in cache:
+                continue
+            cache.add(fen, True)
+            stockfish_engine.set_fen_position(fen)
+            cp = stockfish_engine.get_evaluation()
+            if cp["type"] == "mate":
+                if cp["value"] == 0:
+                    cp = 12800 * np.sign(cp["value"])
+                else:
+                    cp = 12800 / ((abs(cp["value"]) + 1) * np.sign(cp["value"]))
+            else:
+                cp = cp["value"]
+            prob = map_centipawns_to_probability(cp)
+            insert_or_abort(conn, fen, cp, prob, node.encoded)
+            pos_count += 1
+            pbar.update(1)
+            if pos_count % 100 == 0:
+                conn.commit()
+    conn.commit()
+
 
 if __name__ == '__main__':
-    timer_start = time.time()
-    # shards_dir = "C:/Users/janic/Documents"
-    shards_dir = "C:/Users/janic/Downloads"
-    shards = os.listdir(shards_dir)
-    paths = []
-    for file_name in shards:
-        file_name_base, file_extension = os.path.splitext(file_name)
-        if file_extension != ".pgn":
-            continue
-        folder_path = os.path.join(shards_dir, file_name)
-        paths.append(folder_path)
-    for path in paths:
-        shard_parser(path)
-
-    # with Pool(4) as pool:
-    #     pool.map(shard_parser, args)
-    #     pool.close()
-    #     pool.join()
-
-    timer_end = time.time()
-    # convert to minutes or hours if necessary
-    if timer_end - timer_start > 60:
-        if timer_end - timer_start > 3600:
-            print("Processed " + str(len(paths)) + " files" + " in " +
-                  str((timer_end - timer_start) / 3600) + " hours")
-        else:
-            print("Processed " + str(len(paths)) + " files" + " in " +
-                  str((timer_end - timer_start) / 60) + " minutes")
+    create_new(16 * 60*60)
