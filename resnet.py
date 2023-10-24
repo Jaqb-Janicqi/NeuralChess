7527e6f3b7c0b342a90c393351a95cb970673dc5
Jakub Janicki <janicus321@gmail.com>
2023-10-24 14:16:12 +0200

    dynamic layer len, policy switch, sequential fwd

--- resnet.py
+++ resnet.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+from copy import deepcopy
+import torch_directml as dml
 
 
 class ResBlock(nn.Module):
@@ -9,7 +11,7 @@ class ResBlock(nn.Module):
         self.conv2 = nn.Conv2d(num_features, num_features, 3, 1, 1)
         self.bnorm1 = nn.BatchNorm2d(num_features)
         self.bnorm2 = nn.BatchNorm2d(num_features)
-        self.relu = nn.ReLU()
+        self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
         residual = x
@@ -24,56 +26,82 @@ class ResBlock(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, num_blocks, num_features, input_features, policy_size, device="cpu") -> None:
+    def __init__(self, num_blocks, num_features, input_features, policy_size):
         super().__init__()
-        self.device = device
         self.start_block = nn.Sequential(
-            nn.Conv2d(input_features, num_features, 3, padding=1),
+            nn.Conv2d(input_features, num_features, 3, 1, 1),
             nn.BatchNorm2d(num_features),
-            nn.ReLU()
+            nn.ReLU(inplace=True)
         )
         self.blocks = nn.ModuleList(
             [ResBlock(num_features) for _ in range(num_blocks)]
         )
+        self.blocks = nn.Sequential(*self.blocks)
+        p_size, v_size = self.calculate_input_size(
+            num_features, input_features)
         self.policy = nn.Sequential(
             nn.Conv2d(num_features, num_features, 1),
             nn.Conv2d(num_features, num_features, 1),
             nn.BatchNorm2d(num_features),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.Flatten(),
-            nn.Linear(15360, policy_size)
+            nn.Linear(p_size, policy_size)
         )
         self.value = nn.Sequential(
             nn.Conv2d(num_features, num_features, 1),
             nn.BatchNorm2d(num_features),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             nn.Flatten(),
-            nn.Linear(15360, num_features),
-            nn.ReLU(),
+            nn.Linear(v_size, num_features),
+            nn.ReLU(inplace=True),
             nn.Linear(num_features, 1),
             nn.Tanh()
         )
-        self.to(device)
+        self.__disable_policy = False
 
     def forward(self, x):
         x = self.start_block(x)
-        for block in self.blocks:
-            x = block(x)
-        p = self.policy(x)
-        v = self.value(x)
+        x = self.blocks(x)
+        p = self.policy(x).squeeze()
+        v = self.value(x).squeeze()
+        if self.__disable_policy:
+            return v
         return p, v
 
-    def get_tensor_state(self, state):
-        return torch.tensor(state)
-    
-    def get_unbatched_tensor_state(self, state):
-        return torch.tensor(state).unsqueeze(0).to(self.device).float()
-    
-    def to_self(self, data):
-        return data.to(self.device).float()
+    def to_tensor(self, data):
+        return torch.tensor(data).float().unsqueeze(0)
+
+    def batch_to_tensor(self, data):
+        return torch.tensor(data).float()
 
     def get_policy(self, p):
         return torch.softmax(p, 1).squeeze(0).detach().cpu().numpy()
 
     def get_value(self, v):
         return v.item()
+
+    def calculate_input_size(self, num_features, input_features):
+        x = torch.zeros((1, input_features, 8, 8)).float()
+        start_block_copy = deepcopy(self.start_block).float()
+        blocks_copy = deepcopy(self.blocks).float()
+        p_seqence = nn.Sequential(
+            nn.Conv2d(num_features, num_features, 1),
+            nn.Conv2d(num_features, num_features, 1),
+            nn.Flatten()
+        )
+        v_seqence = nn.Sequential(
+            nn.Conv2d(num_features, num_features, 1),
+            nn.Flatten()
+        )
+
+        x = start_block_copy(x)
+        x = blocks_copy(x)
+        p = p_seqence(x)
+        v = v_seqence(x)
+        return p.shape[1], v.shape[1]
+
+    def disable_policy(self):
+        self.__disable_policy = True
+
+    def enable_policy(self):
+        self.__disable_policy = False
