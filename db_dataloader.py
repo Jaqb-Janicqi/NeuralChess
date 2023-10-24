074c9c4463b12fa63934cd25c0e29f43fbff78ac
Jakub Janicki <janicus321@gmail.com>
2023-10-24 14:14:05 +0200

    enable tensor transform and column selection

--- db_dataloader.py
+++ db_dataloader.py
@@ -3,6 +3,7 @@ import random
 import multiprocessing as mp
 import sys
 import numpy as np
+import torch
 
 
 class bit_array():
@@ -25,11 +26,12 @@ class bit_array():
             self.__bit_array[index//64] &= np.uint64(~(1 << (index % 64)))
 
 
-class dataloader(mp.Process):
-    def __init__(self, db_path, table_name, min_index, max_index, num_batches=0,
-                 batch_size=0, slice_size=0, random=True, replace=True, shuffle=True,
-                 specials={}) -> None:
-        """Dataloader for sqlite databases in a seperate process. Either batch_size or num_batches must be set.
+class DataLoader(mp.Process):
+    def __init__(self, db_path, table_name, min_index, max_index,
+                 num_batches=0, batch_size=0, slice_size=0,
+                 random=True, replace=True, shuffle=True,
+                 specials={}, data_cols=[], label_cols=[]) -> None:
+        """DataLoader for sqlite databases in a seperate process. Either batch_size or num_batches must be set.
         If database contains blob data and you want to convert it inside dataloader, 
         you must pass a dictionary with the column names as keys and the conversion functions as values.
         """
@@ -42,17 +44,28 @@ class dataloader(mp.Process):
         self.__random = random
         self.__replace = replace
         self.__shuffle = shuffle
-        self.__min_index = min_index
-        self.__max_index = max_index
+        self.__min_index = int(min_index)
+        self.__max_index = int(max_index)
         self.__slice_size = slice_size
         self.__batch_buffer = mp.Queue(maxsize=2)
         self.__last_idx = 0
-        self.batches_left = mp.Value('i', num_batches)
+        self.__batches_left = 0
         self.__specials = specials
+        self.__data_cols = data_cols
+        self.__label_cols = label_cols
         self.__specials_query_pos = {}
+        self.__data_col_pos = {}
+        self.__label_col_pos = {}
         self.__num_query_cols = None
-        self.__calculate_batch_and_slice_size()
+        self.__setup_attrs()
         self.__get_table_structure()
+        self.__mask_range = int(
+            np.ceil((self.__max_index - self.__min_index) / self.__slice_size))
+        self.__replace_masker = bit_array(self.__mask_range)
+        self.__idx_drawn = 0
+        self.__last_idx_checked = self.__min_index
+        self.__restart_latch = mp.Event()
+        self.__running = True
 
     def __get_table_structure(self):
         conn = sqlite3.connect(self.__db_path)
@@ -64,8 +77,12 @@ class dataloader(mp.Process):
         for row in table_structure:
             if row[1] in self.__specials:
                 self.__specials_query_pos[row[1]] = row[0]
+            if row[1] in self.__data_cols:
+                self.__data_col_pos[row[1]] = row[0]
+            if row[1] in self.__label_cols:
+                self.__label_col_pos[row[1]] = row[0]
 
-    def __calculate_batch_and_slice_size(self):
+    def __setup_attrs(self):
         if self.__max_index == 0:
             conn = sqlite3.connect(self.__db_path)
             cursor = conn.cursor()
@@ -83,7 +100,7 @@ class dataloader(mp.Process):
         if self.__num_batches == 0:
             self.__num_batches = np.ceil(
                 self.__max_index / self.__batch_size).astype(np.uint64)
-            self.batches_left.value = self.__num_batches
+            self.__batches_left = self.__num_batches
         if self.__slice_size == 0:
             self.__slice_size = self.__batch_size
         if self.__last_idx < self.__min_index:
@@ -109,53 +126,68 @@ class dataloader(mp.Process):
     def __create_batch(self):
         batch = []
         missed = 0
-        while len(batch) < self.__batch_size:
+        while len(batch) < self.__batch_size and missed < 10:
             idx = self.__generate_idx
             if idx is None:
-                return None
+                break
             if self.__slice_size == 1:
                 slc = self.__get_single(idx)
             else:
                 slc = self.__get_slice(idx)
-            if len(slc) == 0:
+            if not slc:
                 missed += 1
-                if missed > 10:
-                    raise StopIteration
-                continue
             missed = 0
-            if self.__specials != {}:
-                for row in slc:
-                    new_row = list(row)
-                    for key, func in self.__specials.items():
-                        new_row[self.__specials_query_pos[key]] = func(
-                            new_row[self.__specials_query_pos[key]])
-                    slc[slc.index(row)] = new_row
+
+            if self.__specials:
+                slc = [list(row) for row in slc]
+                for key, func in self.__specials.items():
+                    special_idx = self.__specials_query_pos[key]
+                    for row in slc:
+                        row[special_idx] = func(row[special_idx])
             batch.extend(slc)
 
         if self.__shuffle:
-            random.shuffle(batch)
+            batch = random.sample(batch, len(batch))
 
-        # split batch into columns
-        out_batch = [[] for _ in range(self.__num_query_cols)]
+        # Split batch into columns
+        tmp = [[] for _ in range(self.__num_query_cols)]
         for row in batch:
-            for i in range(self.__num_query_cols):
-                out_batch[i].append(row[i])
+            for i, col in enumerate(row):
+                tmp[i].append(col)
 
-        # convert batch to numpy arrays of correct shape
-        if self.__batch_size > 1:
-            out_batch = [np.asarray(col) for col in out_batch]
-        else:
-            out_batch = [np.asarray(col)[0] for col in out_batch]
+        # filter out data and label columns
+        data = []
+        labels = []
+        for key in self.__data_cols:
+            data.append(tmp[self.__data_col_pos[key]])
+        for key in self.__label_cols:
+            labels.append(tmp[self.__label_col_pos[key]])
+
+        # to tensor
+        data = torch.from_numpy(np.array(data)).squeeze().float()
+        labels = torch.from_numpy(np.array(labels)).squeeze().float()
+        out_batch = (data, labels)
         self.__batch_buffer.put(out_batch)
 
     def run(self):
-        if self.__num_batches == 0:
-            self.__num_batches = sys.maxsize ** 10
-        for _ in range(self.__num_batches):
-            self.__create_batch()
+        while True:
+            if self.__num_batches == 0:
+                self.__num_batches = sys.maxsize ** 10
+            self.__idx_drawn = 0
+            self.__replace_masker = bit_array(self.__mask_range)
+            self.__last_idx = 0
+            self.__idx_drawn = 0
+            self.__last_idx_checked = self.__min_index
+
+            self.__running = True
+            for _ in range(self.__num_batches):
+                self.__create_batch()
+            self.__running = False
+            self.__restart_latch.wait()
+            self.__restart_latch.clear()
 
     def __random_idx(self):
-        return random.randint(self.__min_index, self.__max_index // self.__slice_size)
+        return random.randint(self.__min_index, self.__max_index) // self.__slice_size
 
     def __next_idx(self):
         if self.__last_idx >= self.__max_index // self.__slice_size:
@@ -165,34 +197,34 @@ class dataloader(mp.Process):
         return idx
 
     def __no_replace_idx(self):
-        # create a mask for the random indices
-        mask_range = int(
-            np.ceil((self.__max_index - self.__min_index) / self.__slice_size))
-        replace_masker = bit_array(mask_range)
-        idx_drawn = 0
-
         # draw random indices until 80% of the indices are drawn
-        while self.__max_index * 0.8 > idx_drawn:
+        while self.__max_index // self.__slice_size * 0.8 > self.__idx_drawn:
             idx = self.__random_idx()
-            if replace_masker[idx]:
+            if self.__replace_masker[idx]:
                 continue
-            replace_masker[idx] = 1
-            idx_drawn += 1
-            yield idx
+            self.__replace_masker[idx] = 1
+            self.__idx_drawn += 1
+            return idx
 
         # draw the remaining indices
-        for idx in range(self.__min_index, self.__max_index // self.__slice_size):
-            if replace_masker[idx]:
+        for idx in range(self.__last_idx_checked, int(self.__max_index // self.__slice_size)):
+            if self.__replace_masker[idx]:
                 continue
-            replace_masker[idx] = 1
-            yield idx
+            self.__replace_masker[idx] = 1
+            self.__idx_drawn += 1
+            self.__last_idx_checked = idx
+            return idx
+        return None
+
+    def restart(self):
+        self.__restart_latch.set()
 
     @property
     def __generate_idx(self):
         if not self.__random:
             return self.__next_idx()
         elif not self.__replace:
-            return next(self.__no_replace_idx())
+            return self.__no_replace_idx()
         else:
             return self.__random_idx()
 
@@ -200,10 +232,54 @@ class dataloader(mp.Process):
         return self
 
     def __next__(self):
-        if self.batches_left.value <= 0:
+        if self.__batches_left <= 0:
+            self.__batches_left = self.__num_batches
             raise StopIteration
-        self.batches_left.value -= 1
+        if self.__batch_buffer.empty() and not self.__running:
+            self.restart()
+        self.__batches_left -= 1
         try:
-            return self.__batch_buffer.get(timeout=5*60)
+            return self.__batch_buffer.get(timeout=500)
         except:
             raise StopIteration
+
+    def __len__(self):
+        if self.__num_batches != 0:
+            return self.__num_batches
+        return int(np.ceil((self.__min_index - self.__max_index) / self.__batch_size))
+
+    @property
+    def dataset(self):
+        return self
+
+
+def convert_to_numpy(arr):
+    return np.frombuffer(arr, dtype=np.float32).reshape(16, 8, 8)
+
+
+if __name__ == "__main__":
+    conn = sqlite3.connect('C:/sqlite_chess_db/chess_positions.db')
+    db_size = conn.execute("SELECT COUNT(*) FROM positions").fetchone()[0]
+    conn.close()
+    tmp = DataLoader(
+        db_path='C:/sqlite_chess_db/chess_positions.db',
+        table_name='positions',
+        num_batches=0,
+        batch_size=1024,
+        min_index=1,
+        max_index=db_size*0.1,
+        random=True,
+        replace=False,
+        shuffle=True,
+        slice_size=64,
+        specials={'encoded': convert_to_numpy},
+        data_cols=['encoded'],
+        label_cols=['prob']
+    )
+    tmp.start()
+    for batch in tmp:
+        x, y = batch
+        print(batch[0])
+    print("reset")
+    for batch in tmp:
+        print(batch[0])
