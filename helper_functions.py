0d711a318652c293793164c9c460a883a1e1c7f6
Jakub Janicki <janicus321@gmail.com>
2023-11-20 23:47:24 +0100

    add policy decode and softmax

--- helper_functions.py
+++ helper_functions.py
@@ -1,5 +1,6 @@
 import base64
 import numpy as np
+import torch
 
 
 def encode_a85(byte_rep):
@@ -12,3 +13,14 @@ def decode_a85(byte_rep, dtype=np.uint32):
 
 def decode_position(byte_rep):
     return decode_a85(byte_rep, np.int8).astype(np.float32).reshape(9, 8, 8)
+
+
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
+
+
+def decode_policy(byte_rep):
+    arr = decode_a85(byte_rep, np.uint32).astype(np.float32)
+    arr = arr / arr.sum()
+    return softmax(arr)
