e145c9c6d061912aa3bf074f3b111f9b04b04059
Jakub Janicki <janicus321@gmail.com>
2023-11-20 06:21:47 +0100

    fix not updating distribution in database

--- db_create.py
+++ db_create.py
@@ -7,16 +7,56 @@ import os
 import tqdm
 import time
 import io
+import torch
 
-import yaml
 from mcts_node import Node
-import stockfish
-# from mcts_node import Node
-from mcts_node_ext import Node
+# from mcts_node_ext import Node
 import multiprocessing as mp
 from actionspace import ActionSpace as asp
-import sys
-import base64
+from helper_functions import *
+from collections import OrderedDict
+import psutil
+
+
+UINT32_MAX = 2**32 - 1
+MAX_MEM_USAGE = 10.5*1024*1024*1024   # xGB
+DUMP_SIZE = int(1e3)
+DUMP_QUEUE_SIZE = int(5e4)
+COMMIT_SIZE = int(1e4)
+FILE_START = 0
+FILE_END = None
+
+
+class Position:
+    def __init__(self, encoded, fen, cp, policy, value, sample_count, legal_moves):
+        self.encoded: str = encoded
+        self.fen: str = fen
+        self.cp: int = cp
+        self.policy: np.ndarray = policy
+        self.value: float = value
+        self.sample_count: int = sample_count
+        self.legal_moves: int = legal_moves
+
+    def merge(self, other: 'Position'):
+        if self.sample_count + other.sample_count > UINT32_MAX:
+            return False
+        self.cp = other.cp if abs(other.cp) < abs(self.cp) else self.cp
+        self.value = other.value if abs(
+            other.value) < abs(self.value) else self.value
+        if not self.policy.flags.writeable:
+            self.policy = self.policy.copy()
+        self.policy += other.policy
+        self.sample_count += other.sample_count
+        return True
+
+    def update(self, cp, value, move_id):
+        if self.sample_count + 1 > UINT32_MAX:
+            return False
+        self.cp = cp if abs(cp) < abs(self.cp) else self.cp
+        self.value = value if abs(value) < abs(self.value) else self.value
+        self.policy[move_id] += 1
+        self.sample_count += 1
+        return True
 
 
 def get_centipawns(prob):
@@ -35,21 +75,43 @@ def get_position(conn, encoded):
     return conn.execute("SELECT * FROM positions WHERE encoded = ?", (encoded,)).fetchone()
 
 
-def insert_or_replace_position(conn, encoded, fen, cp, policy, value, sample_count, legal_moves):
+def insert_or_replace_position(conn, item: Position):
     # Insert or replace the position
     conn.execute("""
         INSERT OR REPLACE INTO positions (encoded, fen, cp, policy, value, sample_count, legal_moves)
         VALUES (?, ?, ?, ?, ?, ?, ?)
         ON CONFLICT DO UPDATE SET
-            fen = excluded.fen,
             cp = excluded.cp,
             policy = excluded.policy,
             value = excluded.value,
             sample_count = excluded.sample_count
-    """, (encoded, fen, cp, policy, value, sample_count, legal_moves))
+    """, (item.encoded, item.fen, item.cp, item.policy, item.value, item.sample_count, item.legal_moves))
 
 
-def process_game(line, actionspace, db_dict, verbose=False):
+def fix_db(db_path):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    row_id = 1
+    while True:
+        cursor.execute("SELECT * FROM positions WHERE id = ?", (row_id,))
+        row = cursor.fetchone()
+        if not row:
+            break
+        row_id += 1
+        encoded = row[1]
+        fen = row[2]
+        cp = row[3]
+        policy = decode_a85(row[4])
+        value = row[5]
+        sample_count = np.sum(policy)
+        legal_moves = row[7]
+        item = Position(encoded, fen, cp, policy, value,
+                        sample_count, legal_moves)
+        insert_or_replace_position(conn, item)
+    conn.close()
+
+
+def process_game(line, actionspace: asp, db_dict, verbose=True, rotate=True):
     try:
         pgn = io.StringIO(line)
         game = chess.pgn.read_game(pgn)
@@ -61,9 +123,7 @@ def process_game(line, actionspace, db_dict, verbose=False):
         # game = game.next()  # skip starting position
         while True:
             move_made = game.next().move
-            if game.board().turn == chess.WHITE:
-                fen = game.board().fen()
-            else:
+            if rotate and game.board().turn == chess.BLACK:
                 # flip board such that the king is always on the right
                 fen = game.board().mirror().fen()
                 move_made = chess.Move(
@@ -71,34 +131,34 @@ def process_game(line, actionspace, db_dict, verbose=False):
                     chess.square_mirror(move_made.to_square),
                     move_made.promotion
                 )
+            else:
+                fen = game.board().fen()
 
             cp = game.eval()
             if cp is None:
                 cp = 0
             else:
-                cp = cp.pov(chess.WHITE)
-                cp = cp.score(mate_score=12800)
+                cp = cp.relative.score(mate_score=12800)
             value = map_centipawns_to_probability(cp)
-            encoded = node.encoded.astype(np.int8).tobytes()
+            encoded = encode_a85(node.encoded.astype(np.int8).tobytes())
 
-            # insert into db_dict
             if encoded in db_dict:
-                db_row = db_dict[encoded]
-                db_row[2][actionspace.get_key(move_made)] += 1
-                db_row[4] += 1
-                if abs(db_row[1]) > abs(cp):
-                    db_row[1] = cp
-                    db_row[3] = value
+                db_dict[encoded].update(
+                    cp, value, actionspace.get_key(move_made))
             else:
                 policy = np.zeros(actionspace.size, dtype=np.uint32)
                 policy[actionspace.get_key(move_made)] += 1
-                db_dict[encoded] = [fen, cp, policy, value, 1, game.board().legal_moves.count()]
+                pos = Position(encoded, fen, cp, policy, value, 1,
+                               game.board().legal_moves.count())
+                db_dict[encoded] = pos
 
             # move to next position
             game = game.next()
             if not game:
                 return
             if not game.eval():
+                return
+            if not game.next():
                 return
             # update the tree
             node.add_child(game.move)
@@ -110,40 +170,47 @@ def process_game(line, actionspace, db_dict, verbose=False):
             print(line)
 
 
-def parse_worker(shard_path, queue):
-    db_dict = {}
+def parse_worker(shard_path, queue: mp.Queue, rotate):
+    db_dict = OrderedDict()
     actionspace = asp()
     skip_game = False
     with open(shard_path) as file:
         for line in file:
-            if line == "\n":
+            if line == '\n':
                 continue
-            if line.startswith("WhiteElo"):
-                elo = int(line.split('"')[1])
-                if elo < 2200:
-                    skip_game = True
-                    continue
-            if line.startswith("BlackElo"):
-                elo = int(line.split('"')[1])
-                if elo < 2200:
-                    skip_game = True
-                    continue
+
+            # check game skip unless skip_game is True
+            if skip_game == False:
+                if line.startswith('[WhiteElo'):
+                    elo = int(line.split('"')[1])
+                    if elo < 2200:
+                        skip_game = True
+                        continue
+
+                if line.startswith('[BlackElo'):
+                    elo = int(line.split('"')[1])
+                    if elo < 2200:
+                        skip_game = True
+                        continue
+
+                if line.startswith('[TimeControl'):
+                    time_control = line.split('"')[1]
+                    total_time = int(time_control.split('+')[0])
+                    if total_time < 180:
+                        skip_game = True
+                        continue
+
             if line.startswith("1."):
                 if skip_game:
                     skip_game = False
                     continue
                 if '[%eval' in line:
-                    process_game(line, actionspace, db_dict)
-    for encoded, (fen, cp, policy, value, sample_count, legal_moves) in db_dict.items():
-        queue.put((encoded, fen, cp, policy, value, sample_count, legal_moves))
+                    process_game(line, actionspace, db_dict, rotate=rotate)
 
-
-def encode_a85(byte_rep):
-    return base64.a85encode(byte_rep).decode()
-
-
-def decode_a85(byte_rep):
-    return np.frombuffer(base64.a85decode(byte_rep), dtype=np.uint32)
+    # dump items
+    for _ in range(len(db_dict)):
+        _, item = db_dict.popitem(last=False)
+        queue.put(item)
 
 
 def prune_database(conn):
@@ -158,70 +225,89 @@ def should_prune(db_path):
     return False
 
 
-def db_writer(queue, db_path, verbose=False):
-    db_dict = {}
-    conn = sqlite3.connect(db_path)
-    row_count = 0
-    exit_flag = False
-    while not exit_flag:
-        # combine rows from workers in memory
-        while len(db_dict) < 100000:
-            try:
-                q_item = queue.get()
-                if q_item is None:
-                    exit_flag = True
-                    break
-                row_count += 1
-                encoded, fen, cp, policy, value, sample_count, legal_moves = q_item
-                if encoded in db_dict:
-                    db_row = db_dict[encoded]
-                    # update distribution of current position with move played
-                    db_row[2] += policy
-                    db_row[4] += sample_count
-                    if abs(db_row[1]) > abs(cp):
-                        db_row[1] = cp
-                        db_row[3] = value
-                else:
-                    db_dict[encoded] = [fen, cp, policy, value, sample_count, legal_moves]
-            except Exception as e:
-                if verbose:
-                    print(e)
-                    print(q_item)
+def write_to_db(conn, item: Position):
+    pos = get_position(conn, item.encoded)
+    if pos:
+        pos = Position(*(pos[1:]))
+        pos.policy = decode_a85(pos.policy)
+        if not item.merge(pos):
+            return
+    item.policy = encode_a85(item.policy)
+    insert_or_replace_position(conn, item)
+    del item, pos
 
-        # write to db
-        for encoded, (fen, cp, policy, value, sample_count, legal_moves) in db_dict.items():
-            encoded = encode_a85(encoded)
-            row = get_position(conn, encoded)
-            if row:
-                cp = min(row[2], cp)
-                value = min(row[4], value)
-                # prevent overflow
-                if row[5] + sample_count < 2**32 - 1:
-                    policy = decode_a85(row[3]) + policy
-                    policy = encode_a85(policy)
-                    sample_count = row[5] + sample_count
-                else:
-                    sample_count = 2**32 - 1
-                    policy = row[3]
-            else:
-                policy = encode_a85(policy)
-            insert_or_replace_position(
-                conn, encoded, fen, cp, policy, value, sample_count, legal_moves)
-        db_dict = {}
-        conn.commit()
-        if should_prune(db_path):
-            prune_database(conn, db_path)
+
+def db_worker(queue: mp.Queue, db_path: str):
+    conn = sqlite3.connect(db_path)
+    pending_commit = 0
+    while True:
+        item = queue.get()
+        if item is None:
+            break
+
+        write_to_db(conn, item)
+        pending_commit += 1
+
+        if pending_commit >= COMMIT_SIZE:
+            conn.commit()
+            pending_commit = 0
+    conn.commit()
     conn.close()
 
 
+def add_item(dictionary, item: Position):
+    if item.encoded in dictionary:
+        dictionary[item.encoded].merge(item)
+        del item
+    else:
+        dictionary[item.encoded] = item
+
+
+def data_worker(parser_queue: mp.Queue, db_queue: mp.Queue):
+    db_dict = OrderedDict()
+    p_id = mp.current_process().pid
+
+    while True:
+        mem_usage = psutil.Process(p_id).memory_info().vms
+        # start writing to db if mem usage is too high
+        if mem_usage > MAX_MEM_USAGE:
+            for _ in range(DUMP_SIZE):
+                try:
+                    _, item = db_dict.popitem(last=False)
+                except KeyError:
+                    break
+                db_queue.put(item)
+            continue
+
+        # wait for item from parser
+        while parser_queue.empty():
+            time.sleep(1)
+
+        # get item from parser
+        q_item = parser_queue.get()
+        # exit if parsing has finished
+        if q_item is None:
+            break
+
+        # add item to db_dict
+        add_item(db_dict, q_item)
+
+    # dump remaining items
+    for _ in range(len(db_dict)):
+        _, item = db_dict.popitem(last=False)
+        db_queue.put(item)
+    db_queue.put(None)
+
+
 def parse(shard_path) -> None:
+    rotate_board = False
     tic = time.time()
-    db_path = 'C:/sqlite_chess_db/lichess2200.db'
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS positions (
-            encoded BLOB PRIMARY KEY NOT NULL UNIQUE,
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            encoded BLOB NOT NULL UNIQUE,
             fen TEXT NOT NULL,
             cp INTEGER NOT NULL,
             policy BLOB NOT NULL,
@@ -233,23 +319,36 @@ def parse(shard_path) -> None:
     conn.commit()
     conn.close()
     files = os.listdir(shard_path)
-    files = files[:1500]    #process 1500 files
-    queue = mp.Manager().Queue(10000)
-    db_worker = mp.Process(target=db_writer, args=(queue, db_path))
-    db_worker.start()
+    if FILE_END is not None:
+        file_end = FILE_END
+    else:
+        file_end = len(files)
+    files = files[FILE_START:file_end]
+    parser_queue = mp.Manager().Queue(int(1e3))
+    db_queue = mp.Manager().Queue(DUMP_QUEUE_SIZE)
+    db_writer = mp.Process(target=db_worker, args=(db_queue, db_path))
+    db_writer.start()
+    data_agregator = mp.Process(target=data_worker, args=(
+        parser_queue, db_queue))
+    data_agregator.start()
     p_size = mp.cpu_count()
     # p_size = 1
     with mp.Pool(processes=p_size) as pool:
+        pbar = tqdm.tqdm(total=len(files), desc="Parsing files",
+                         leave=False, dynamic_ncols=True, position=0)
         for file in files:
             if file.endswith(".pgn"):
                 pool.apply_async(parse_worker, args=(
-                    shard_path + file, queue))
+                    shard_path + file, parser_queue, rotate_board), callback=lambda _: pbar.update())
         pool.close()
         pool.join()
-    queue.put(None)
-    db_worker.join()
+    pbar.close()
+    parser_queue.put(None)
+    data_agregator.join()
+    db_queue.put(None)
+    db_writer.join()
     toc = time.time()
-    print(f"Processed {len(files)} files in {toc - tic} seconds")
+    print(f"Processed {len(files)} files in {(toc - tic)/60} minutes")
 
 
 def convert_to_numpy(arr):
@@ -257,7 +356,6 @@ def convert_to_numpy(arr):
 
 
 def display():
-    db_path = 'C:/sqlite_chess_db/lichess2200.db'
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     cursor.execute(
@@ -267,6 +365,7 @@ def display():
     for row in rows:
         fen = row[1]
         print(fen)
+        print(row[2])
         policy = decode_a85(row[3])
         played = np.argwhere(policy > 0)
         actionspace = asp()
@@ -283,6 +382,36 @@ def display():
     conn.close()
 
 
+def count():
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute("SELECT COUNT(*) FROM positions")
+    rows = cursor.fetchall()
+    print(rows)
+    cursor.execute(
+        "SELECT COUNT(*) FROM positions where sample_count > legal_moves or sample_count > 20")
+    rows = cursor.fetchall()
+    print(rows)
+    cursor.execute(
+        "SELECT COUNT(*) FROM positions where sample_count > legal_moves or sample_count > 10")
+    rows = cursor.fetchall()
+    print(rows)
+    cursor.execute(
+        "SELECT COUNT(*) FROM positions where sample_count > legal_moves or sample_count > 5")
+    rows = cursor.fetchall()
+    print(rows)
+    cursor.execute(
+        "SELECT COUNT(*) FROM positions where sample_count > legal_moves or sample_count > 2")
+    rows = cursor.fetchall()
+    print(rows)
+    conn.close()
+
+
+db_path = 'C:/sqlite_chess_db/lichess2200.db'
+
 if __name__ == '__main__':
+    # fix_db(db_path)
+    count()
     # display()
-    parse('E:/lichess_shards/lichess_db_standard_rated_2023-9/')
+    # parse('E:/lichess_shards/lichess_db_standard_rated_2023-9/')
+    # parse('E:/lichess_shards/lichess_db_standard_rated_2023-10/')
