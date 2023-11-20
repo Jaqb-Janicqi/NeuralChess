09881009ee65d3eb82c0d764cab1fdaad3f53e4f
Jakub Janicki <janicus321@gmail.com>
2023-11-20 06:26:24 +0100

    add wd anneal

--- scheduler.py
+++ scheduler.py
@@ -5,7 +5,7 @@ import torch
 
 class Scheduler():
     def __init__(self, optimizer, num_steps, max_lr=1e-3, min_lr=1e-5,
-                 pct_start=0.2, pct_max=0.1, annealing='cosine') -> None:
+                 pct_start=0.2, pct_max=0.1, annealing='cosine', conditional_wd=False) -> None:
         self.__optimizer: torch.optim.Optimizer = optimizer
         self.__num_steps = num_steps
         self.__max_lr = max_lr
@@ -13,17 +13,19 @@ class Scheduler():
         self.__pct_start = pct_start
         self.__pct_max = pct_max
         self.__annealing = annealing
+        self.__conditional_wd = conditional_wd
+        self.__wd = self.__optimizer.param_groups[0]['weight_decay']
         self.__step_num = 0
         self.restart()
 
     def lr_function(self, x):
-        if x <= self.__num_steps * self.__pct_start:
+        if x <= self.__num_steps * self.__pct_start and self.__pct_start > 0:
             # increase the learning rate up to max_lr starting at min_lr
             lr_range = self.__max_lr - self.__min_lr
             lr_func = (
                 1 + np.cos(np.pi * x / (self.__num_steps * self.__pct_start) + np.pi)) / 2
             return self.__min_lr + lr_range * lr_func
-        elif x <= self.__num_steps * (self.__pct_start + self.__pct_max):
+        elif x <= self.__num_steps * (self.__pct_start + self.__pct_max) and self.__pct_max > 0:
             # keep the learning rate at max_lr
             return self.__max_lr
         else:
@@ -49,6 +51,11 @@ class Scheduler():
         self.__step_num += 1
         self.__optimizer.param_groups[0]['lr'] = self.lr_function(
             self.__step_num)
+        if self.__conditional_wd:
+            if self.__optimizer.param_groups[0]['lr'] < self.min_lr:
+                self.__optimizer.param_groups[0]['weight_decay'] = 0
+            else:
+                self.__optimizer.param_groups[0]['weight_decay'] = self.__wd
 
     def restart(self):
         self.__step_num = 0
@@ -84,15 +91,15 @@ class Scheduler():
     @property
     def max_lr(self):
         return self.__max_lr
-    
+
     @max_lr.setter
     def max_lr(self, max_lr):
         self.__max_lr = max_lr
-    
+
     @property
     def min_lr(self):
         return self.__min_lr
-    
+
     @min_lr.setter
     def min_lr(self, min_lr):
         self.__min_lr = min_lr
@@ -101,10 +108,15 @@ class Scheduler():
 if __name__ == "__main__":
     opt = torch.optim.ASGD([torch.zeros(1)], lr=1e-3)
     params = opt.param_groups[0]
-    sch = Scheduler(opt, 4000, 
-            max_lr=1e-2,
-            min_lr=1e-8,
-            pct_start=0.2,
-            pct_max=0.05,
-            annealing='cosine',)
-    sch.max_lr = 1e-3
+    sch = Scheduler(opt, 4000,
+                    max_lr=1e-1,
+                    min_lr=5e-2,
+                    pct_start=0.1,
+                    pct_max=0.1,
+                    annealing='cosine',)
+    lrs = []
+    for i in range(4000):
+        sch.step()
+        lrs.append(params['lr'])
+    plt.plot(lrs)
+    plt.show(block=True)
